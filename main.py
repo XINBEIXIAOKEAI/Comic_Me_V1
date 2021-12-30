ad333140e925339b53f0e7c6aee9c1f8f1752f58
XINBEIXIAOKEAI <95857482+XINBEIXIAOKEAI@users.noreply.github.com>
2021-12-30 21:21:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def main():
                 with col2:
                     st.image(prediction)
         elif mode == '油畫風格':
-            Image = st.file_uploader('在這上傳您的檔案',type=['jpg','jpeg','png'])
+            img = st.file_uploader('在這上傳您的檔案',type=['jpg','jpeg','png'])
             st.image(Image)
             def asciiart(in_f, SC, GCF,  out_f, color1='black', color2='blue', bgcolor='white'):
             
@@ -95,8 +95,8 @@ def main():
                 WCF = letter_height/letter_width
 
                 #open the input file
-                Image = Image.open(in_f)
-                st.image(Image)
+                img = Image.open(in_f)
+                st.image(img)
 
                 #Based on the desired output image size, calculate how many ascii letters are needed on the width and height
                 widthByLetter=round(img.size[0]*SC*WCF)
@@ -104,16 +104,16 @@ def main():
                 S = (widthByLetter, heightByLetter)
 
                 #Resize the image based on the symbol width and height
-                Image = Image.resize(S)
+                img = Image.resize(S)
 
                 #Get the RGB color values of each sampled pixel point and convert them to graycolor using the average method.
                 # Refer to https://www.johndcook.com/blog/2009/08/24/algorithms-convert-color-grayscale/ to know about the algorithm
-                Image = np.sum(np.asarray(img), axis=2)
+                img = np.sum(np.asarray(img), axis=2)
 
                 # Normalize the results, enhance and reduce the brightness contrast. 
                 # Map grayscale values to bins of symbols
-                Image -= Image.min()
-                Image = (1.0 - Image/Image.max())**GCF*(chars.size-1)
+                img -= Image.min()
+                img = (1.0 - Image/Image.max())**GCF*(chars.size-1)
 
                 # Generate the ascii art symbols 
                 lines = ("\n".join( ("".join(r) for r in chars[Image.astype(int)]) )).split("\n")
@@ -211,6 +211,9 @@ def main():
                     
                     
         elif mode == '油畫風格':
+            url = st.text_input('網址連結')
+            response = requests.get(url)
+            img = (response.content)
             def asciiart(in_f, SC, GCF,  out_f, color1='black', color2='blue', bgcolor='white'):
 
                 # The array of ascii symbols from white to black
@@ -224,10 +227,7 @@ def main():
 
                 WCF = letter_height/letter_width
 
-                #open the input file
-                url = st.text_input('網址連結')
-                response = requests.get(url)
-                img = (response.content)
+
 
 
                 #Based on the desired output image size, calculate how many ascii letters are needed on the width and height
