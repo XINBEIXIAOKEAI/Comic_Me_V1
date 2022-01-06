0ad2dbee897c0c5975b412a22a7018dceeacf2ad
XINBEIXIAOKEAI <95857482+XINBEIXIAOKEAI@users.noreply.github.com>
2022-01-06 19:55:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def main():
         gamma = st.sidebar.slider('Gamma 調整', min_value=0.1, max_value=3.0,value=1.0,step=0.1) # change the value here to get different result
 
         if mode == '漫畫風格':
-        Image = st.file_uploader('在這上傳您的檔案',type=['jpg','jpeg','png'])
+            Image = st.file_uploader('在這上傳您的檔案',type=['jpg','jpeg','png'])
             if Image is not None:
                 col1, col2 = st.beta_columns(2)
                 Image = Image.read()
