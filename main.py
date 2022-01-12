797129ecf11ef597eaa195e2be7affce4cb42caf
XINBEIXIAOKEAI <95857482+XINBEIXIAOKEAI@users.noreply.github.com>
2022-01-12 14:16:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -103,6 +103,18 @@ def imgGen2(img1):
 	asciiart(inputf, SC, GCF, "results_pink.png","blue","pink")
 	img = Image.open(img1)
 	img2 = Image.open('results.png').resize(img.size)
+def load_image(filename, size=(512,512)):
+	# load image with the preferred size
+	pixels = load_img(filename, target_size=size)
+	# convert to numpy array
+	pixels = img_to_array(pixels)
+	# scale from [0,255] to [-1,1]
+	pixels = (pixels - 127.5) / 127.5
+	# reshape to 1 sample
+	pixels = expand_dims(pixels, 0)
+	return pixels
+
+
 	
 def main():
     st.set_page_config(layout="wide")
@@ -114,17 +126,6 @@ def main():
         st.write("**AutoCrop** help the model by finding and cropping the biggest face it can find.")
         st.write("**Gamma Adjustment** can be used to lighten/darken the image")
     comic_model = model_load()
-            def load_image(filename, size=(512,512)):
-	# load image with the preferred size
-	            pixels = load_img(filename, target_size=size)
-	# convert to numpy array
-	            pixels = img_to_array(pixels)
-	# scale from [0,255] to [-1,1]
-	            pixels = (pixels - 127.5) / 127.5
-	# reshape to 1 sample
-	            pixels = expand_dims(pixels, 0)
-	            return pixels
-
 
 def imgGen2(img1):
     inputf = img1  # Input image file name
