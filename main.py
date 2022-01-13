c4dd86eb07f80a4814b252d2cddc91a268597bb1
XINBEIXIAOKEAI <95857482+XINBEIXIAOKEAI@users.noreply.github.com>
2022-01-13 20:39:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -82,39 +82,39 @@ def main():
     def asciiart(in_f, SC, GCF,  out_f, color1='black', color2='blue', bgcolor='white'):
 
 # The array of ascii symbols from white to black
-	chars = np.asarray(list(' .,:irs?@9B&#'))
+        chars = np.asarray(list(' .,:irs?@9B&#'))
 
 	# Load the fonts and then get the the height and width of a typical symbol 
 	# You can use different fonts here
-	font = ImageFont.load_default()
-	letter_width = font.getsize("x")[0]
-	letter_height = font.getsize("x")[1]
+        font = ImageFont.load_default()
+        letter_width = font.getsize("x")[0]
+        letter_height = font.getsize("x")[1]
 
-	WCF = letter_height/letter_width
+        WCF = letter_height/letter_width
 
 	#open the input file
-	img = Image.open(in_f)
+        img = Image.open(in_f)
 
 
 	#Based on the desired output image size, calculate how many ascii letters are needed on the width and height
-	widthByLetter=round(img.size[0]*SC*WCF)
-	heightByLetter = round(img.size[1]*SC)
-	S = (widthByLetter, heightByLetter)
+        widthByLetter=round(img.size[0]*SC*WCF)
+        heightByLetter = round(img.size[1]*SC)
+        S = (widthByLetter, heightByLetter)
 
 	#Resize the image based on the symbol width and height
-	img = img.resize(S)
+        img = img.resize(S)
 
 	#Get the RGB color values of each sampled pixel point and convert them to graycolor using the average method.
 	# Refer to https://www.johndcook.com/blog/2009/08/24/algorithms-convert-color-grayscale/ to know about the algorithm
-	img = np.sum(np.asarray(img), axis=2)
+        img = np.sum(np.asarray(img), axis=2)
 
 	# Normalize the results, enhance and reduce the brightness contrast. 
 	# Map grayscale values to bins of symbols
-	img -= img.min()
-	img = (1.0 - img/img.max())**GCF*(chars.size-1)
+        img -= img.min()
+        img = (1.0 - img/img.max())**GCF*(chars.size-1)
 
 	# Generate the ascii art symbols 
-	lines = ("\n".join( ("".join(r) for r in chars[img.astype(int)]) )).split("\n")
+        lines = ("\n".join( ("".join(r) for r in chars[img.astype(int)]) )).split("\n")
 
 	# Create gradient color bins
 	nbins = len(lines)
