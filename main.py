dfbff058840a335268707355c03fc0636bc6c5ca
XINBEIXIAOKEAI <95857482+XINBEIXIAOKEAI@users.noreply.github.com>
2022-01-13 18:27:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -82,14 +82,14 @@ def imgGen2(img1):
             Image = tf.image.decode_image(Image, channels=3).numpy()                  
             Image = adjust_gamma(Image, gamma=gamma)
             with col1:
-			st.image(Image)
-		input_image = loadtest(Image,cropornot=Autocrop)
-		prediction = comic_model(input_image, training=True)
-		prediction = tf.squeeze(prediction,0)
-		prediction = prediction* 0.5 + 0.5
-		prediction = tf.image.resize(prediction, 
-			[outputsize, outputsize],
-			method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+                st.image(Image)
+            input_image = loadtest(Image,cropornot=Autocrop)
+            prediction = comic_model(input_image, training=True)
+            prediction = tf.squeeze(prediction,0)
+            prediction = prediction* 0.5 + 0.5
+            prediction = tf.image.resize(prediction, 
+                [outputsize, outputsize],
+                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 	    prediction=  prediction.numpy()
 	    with col2:
 		st.image(prediction)
