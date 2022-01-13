dbfefb2ad74b930b2c77f282ab4f3057378ea49d
XINBEIXIAOKEAI <95857482+XINBEIXIAOKEAI@users.noreply.github.com>
2022-01-13 18:16:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def imgGen2(img1):
         Autocrop = st.sidebar.checkbox('自動裁剪照片',value=True) 
         gamma = st.sidebar.slider('Gamma 調整', min_value=0.1, max_value=3.0,value=1.0,step=0.1) # change the value here to get different result
         
-		Image = st.file_uploader('在這上傳您的檔案',type=['jpg','jpeg','png'])
+        Image = st.file_uploader('在這上傳您的檔案',type=['jpg','jpeg','png'])
 		if file_uploader is not None:
 		    col1, col2 = st.beta_columns(2)
 		    Image = Image.read()
