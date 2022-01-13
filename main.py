61063c42ebdd0824b9473ac5a7358b8b8196804f
XINBEIXIAOKEAI <95857482+XINBEIXIAOKEAI@users.noreply.github.com>
2022-01-13 18:21:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -94,6 +94,7 @@ def imgGen2(img1):
 		st.image(prediction)
 
 	Image = st.file_uploader('在這上傳您的檔案',type=['jpg','jpeg','png'])
+
 	if file_uploader is not None:
 		def asciiart(in_f, SC, GCF,  out_f, color1='black', color2='blue', bgcolor='white'):
 		    chars = np.asarray(list(' .,:irs?@9B&#'))
