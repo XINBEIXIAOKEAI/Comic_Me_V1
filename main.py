2f006dc9e493a72b4c66dcd7bc12e857886f6415
XINBEIXIAOKEAI <95857482+XINBEIXIAOKEAI@users.noreply.github.com>
2022-01-13 19:46:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -74,8 +74,8 @@ def main():
             prediction = tf.squeeze(prediction,0)
             prediction = prediction* 0.5 + 0.5
             prediction = tf.image.resize(prediction, 
-                [outputsize, outputsize],
-                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+                           [outputsize, outputsize],
+                           method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
             prediction=  prediction.numpy()
             with col2:
                 st.image(prediction)
