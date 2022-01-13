1dddac4d5037f2c6e201ed0752ed840a5ef134f3
XINBEIXIAOKEAI <95857482+XINBEIXIAOKEAI@users.noreply.github.com>
2022-01-13 21:29:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -152,31 +152,30 @@ def main():
         
         Image = st.file_uploader('在這上傳您的檔案',type=['jpg','jpeg','png'])
         if Image is not None:
-		
-	#src_image = load_image(Image)
-		image = Image.open(Image)	
 
-		st.image(Image, caption='Input Image', use_column_width=True)
+            col1, col2 = st.beta_columns(2)
+            Image = Image.read()
+            Image = tf.image.decode_image(Image, channels=3).numpy()                  
+            Image = adjust_gamma(Image, gamma=gamma)
+            with col1:
+                st.image(Image)
+            input_image = loadtest(Image,cropornot=Autocrop)
+            prediction = comic_model(input_image, training=True)
+            prediction = tf.squeeze(prediction,0)
+            prediction = prediction* 0.5 + 0.5
+            prediction = tf.image.resize(prediction, 
+                           [outputsize, outputsize],
+                           method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+            prediction=  prediction.numpy()
+            with col2:
+                st.image(prediction)
+			#src_image = load_image(Image)
+            image = Image.open(Image)	
+
+            st.image(Image, caption='Input Image', use_column_width=True)
 			#st.write(os.listdir())
-		im = imgGen2(Image)	
-		st.image(im, caption='ASCII art', use_column_width=True) 
-
-		col1, col2 = st.beta_columns(2)
-		Image = Image.read()
-		Image = tf.image.decode_image(Image, channels=3).numpy()                  
-		Image = adjust_gamma(Image, gamma=gamma)
-		with col1:
-		    st.image(Image)
-		input_image = loadtest(Image,cropornot=Autocrop)
-		prediction = comic_model(input_image, training=True)
-		prediction = tf.squeeze(prediction,0)
-		prediction = prediction* 0.5 + 0.5
-		prediction = tf.image.resize(prediction, 
-		    [outputsize, outputsize],
-		    method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-		    prediction=  prediction.numpy()
-		with col2:
-		    st.image(prediction)
+            im = imgGen2(Image)	
+            st.image(im, caption='ASCII art', use_column_width=True)
 
     elif choice == 'URL':
 	st.sidebar.header('配置')
