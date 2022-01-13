82458555776db0346942b444b7343933c8628e66
XINBEIXIAOKEAI <95857482+XINBEIXIAOKEAI@users.noreply.github.com>
2022-01-13 20:29:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -78,130 +78,136 @@ def main():
             prediction=  prediction.numpy()
             with col2:
                 st.image(prediction)
-                def asciiart(in_f, SC, GCF,  out_f, color1='black', color2='blue', bgcolor='white'):
-                 chars = np.asarray(list(' .,:irs?@9B&#'))
+                
+	def asciiart(in_f, SC, GCF,  out_f, color1='black', color2='blue', bgcolor='white'):
 
-		    # Load the fonts and then get the the height and width of a typical symbol 
-		    # You can use different fonts here
-                 font = ImageFont.load_default()
-                 letter_width = font.getsize("x")[0]
-                 letter_height = font.getsize("x")[1]
+    # The array of ascii symbols from white to black
+		chars = np.asarray(list(' .,:irs?@9B&#'))
 
-                 WCF = letter_height/letter_width
+		# Load the fonts and then get the the height and width of a typical symbol 
+		# You can use different fonts here
+		font = ImageFont.load_default()
+		letter_width = font.getsize("x")[0]
+		letter_height = font.getsize("x")[1]
 
-		    #open the input file
-                 img = Image.open(in_f)
-                 st.image(img)
+		WCF = letter_height/letter_width
+
+		#open the input file
+		img = Image.open(in_f)
 
 
-		    #Based on the desired output image size, calculate how many ascii letters are needed on the width and height
-                 widthByLetter=round(img.size[0]*SC*WCF)
-                 heightByLetter = round(img.size[1]*SC)
-                 S = (widthByLetter, heightByLetter)
+		#Based on the desired output image size, calculate how many ascii letters are needed on the width and height
+		widthByLetter=round(img.size[0]*SC*WCF)
+		heightByLetter = round(img.size[1]*SC)
+		S = (widthByLetter, heightByLetter)
 
-		    #Resize the image based on the symbol width and height
-                 img = img.resize(S)
+		#Resize the image based on the symbol width and height
+		img = img.resize(S)
 
-		    #Get the RGB color values of each sampled pixel point and convert them to graycolor using the average method.
-		    # Refer to https://www.johndcook.com/blog/2009/08/24/algorithms-convert-color-grayscale/ to know about the algorithm
-                 img = np.sum(np.asarray(img), axis=2)
+		#Get the RGB color values of each sampled pixel point and convert them to graycolor using the average method.
+		# Refer to https://www.johndcook.com/blog/2009/08/24/algorithms-convert-color-grayscale/ to know about the algorithm
+		img = np.sum(np.asarray(img), axis=2)
 
-		    # Normalize the results, enhance and reduce the brightness contrast. 
-		    # Map grayscale values to bins of symbols
-                 img -= img.min()
-                 img = (1.0 - img/img.max())**GCF*(chars.size-1)
+		# Normalize the results, enhance and reduce the brightness contrast. 
+		# Map grayscale values to bins of symbols
+		img -= img.min()
+		img = (1.0 - img/img.max())**GCF*(chars.size-1)
 
-		    # Generate the ascii art symbols 
-                 lines = ("\n".join( ("".join(r) for r in chars[img.astype(int)]) )).split("\n")
+		# Generate the ascii art symbols 
+		lines = ("\n".join( ("".join(r) for r in chars[img.astype(int)]) )).split("\n")
 
-		    # Create gradient color bins
-                 nbins = len(lines)
-		    #colorRange =list(Color(color1).range_to(Color(color2), nbins))
+		# Create gradient color bins
+		nbins = len(lines)
+		#colorRange =list(Color(color1).range_to(Color(color2), nbins))
 
-		    #Create an image object, set its width and height
-                 newImg_width= letter_width *widthByLetter
-                 newImg_height = letter_height * heightByLetter
-                 newImg = Image.new("RGBA", (newImg_width, newImg_height), bgcolor)
-                 draw = ImageDraw.Draw(newImg)
+		#Create an image object, set its width and height
+		newImg_width= letter_width *widthByLetter
+		newImg_height = letter_height * heightByLetter
+		newImg = Image.new("RGBA", (newImg_width, newImg_height), bgcolor)
+		draw = ImageDraw.Draw(newImg)
 
-		    # Print symbols to image
-                 leftpadding=0
-                 y = 0
-                 lineIdx=0
-                 for line in lines:
-                     color = 'blue'
-                     lineIdx +=1
+		# Print symbols to image
+		leftpadding=0
+		y = 0
+		lineIdx=0
+		for line in lines:
+		color = 'blue'
+		lineIdx +=1
 
-                     draw.text((leftpadding, y), line, '#0000FF', font=font)
-                     y += letter_height
+		draw.text((leftpadding, y), line, '#0000FF', font=font)
+		y += letter_height
 
-		    # Save the image file
+		# Save the image file
 
-		    #out_f = out_f.resize((1280,720))
-                 newImg.save(out_f)
-        def imgGen2(img1):
-             inputf = img1  # Input image file name
+		#out_f = out_f.resize((1280,720))
+		newImg.save(out_f)
 
-             SC = 0.1    # pixel sampling rate in width
-             GCF= 2      # contrast adjustment
 
-             asciiart(inputf, SC, GCF, "results.png")   #default color, black to blue
-             asciiart(inputf, SC, GCF, "results_pink.png","blue","pink")
-             img = Image.open(img1)
-             img2 = Image.open('results.png').resize(img.size)
-        def load_image(filename, size=(512,512)):
+		def load_image(filename, size=(512,512)):
 		# load image with the preferred size
-             pixels = load_img(filename, target_size=size)
+		pixels = load_img(filename, target_size=size)
 		# convert to numpy array
-             pixels = img_to_array(pixels)
+		pixels = img_to_array(pixels)
 		# scale from [0,255] to [-1,1]
-             pixels = (pixels - 127.5) / 127.5
+		pixels = (pixels - 127.5) / 127.5
 		# reshape to 1 sample
-             pixels = expand_dims(pixels, 0)
-             return pixels
-	    #src_image = load_image(file_uploader)
-             image = Image.open(file_uploader)	
+		pixels = expand_dims(pixels, 0)
+		return pixels
 
-             st.image(file_uploader, caption='Input Image', use_column_width=True)
-	    #st.write(os.listdir())
-             im = imgGen2(file_uploader)	
-             st.image(im, caption='ASCII art', use_column_width=True)
-    #src_image = load_image(file_uploader)
-             image = Image.open(file_uploader)	
 
-             st.image(file_uploader, caption='Input Image', use_column_width=True)
-    #st.write(os.listdir())
-             im = imgGen2(file_uploader)	
-             st.image(im, caption='ASCII art', use_column_width=True) 
+		def imgGen2(img1):
+		inputf = img1  # Input image file name
 
-		
-    elif choice == 'URL':
-        st.sidebar.header('配置')
+		SC = 0.1    # pixel sampling rate in width
+		GCF= 2      # contrast adjustment
 
-        outputsize = st.sidebar.selectbox('輸出尺寸', [384,512,768])
-        Autocrop = st.sidebar.checkbox('自動裁剪照片',value=True) 
-        gamma = st.sidebar.slider('Gamma 調整', min_value=0.1, max_value=3.0,value=1.0,step=0.1) # change the value here to get different result
-        
-         
-        url = st.text_input('網址連結')
-        response = requests.get(url)
-        Image = (response.content)
-        if Image is not None:
-            col1, col2 = st.beta_columns(2)
-            Image = tf.image.decode_image(Image).numpy()
-            Image = adjust_gamma(Image, gamma=gamma)
-            with col1:
-                st.image(Image)
-            text_input = loadtest(Image,cropornot=Autocrop)
-            prediction = comic_model(text_input, training=True)
-            prediction = tf.squeeze(prediction,0)
-            prediction = prediction* 0.5 + 0.5
-            prediction = tf.image.resize(prediction, 
-                         [outputsize, outputsize],
-                        method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-            prediction=  prediction.numpy()
-            with col2:
-                st.image(prediction)
+		asciiart(inputf, SC, GCF, "results.png")   #default color, black to blue
+		asciiart(inputf, SC, GCF, "results_pink.png","blue","pink")
+		img = Image.open(img1)
+		img2 = Image.open('results.png').resize(img.size)
+		#img2.save('result.png')
+		#img3 = Image.open('results_pink.png').resize(img.size)
+		#img3.save('resultp.png')
+		return img2	
+
+
+		if Image is not None:
+		#src_image = load_image(Image)
+		image = Image.open(Image)	
+
+		st.image(Image, caption='Input Image', use_column_width=True)
+		#st.write(os.listdir())
+		im = imgGen2(Image)	
+		st.image(im, caption='ASCII art', use_column_width=True) 
+
+
+	    elif choice == 'URL':
+		st.sidebar.header('配置')
+
+		outputsize = st.sidebar.selectbox('輸出尺寸', [384,512,768])
+		Autocrop = st.sidebar.checkbox('自動裁剪照片',value=True) 
+		gamma = st.sidebar.slider('Gamma 調整', min_value=0.1, max_value=3.0,value=1.0,step=0.1) # change the value here to get different result
+
+
+		url = st.text_input('網址連結')
+		response = requests.get(url)
+		Image = (response.content)
+		if Image is not None:
+		    col1, col2 = st.beta_columns(2)
+		    Image = tf.image.decode_image(Image).numpy()
+		    Image = adjust_gamma(Image, gamma=gamma)
+		    with col1:
+			st.image(Image)
+		    text_input = loadtest(Image,cropornot=Autocrop)
+		    prediction = comic_model(text_input, training=True)
+		    prediction = tf.squeeze(prediction,0)
+		    prediction = prediction* 0.5 + 0.5
+		    prediction = tf.image.resize(prediction, 
+				 [outputsize, outputsize],
+				method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+		    prediction=  prediction.numpy()
+		    with col2:
+			st.image(prediction)
  
 
 if __name__ == '__main__':
