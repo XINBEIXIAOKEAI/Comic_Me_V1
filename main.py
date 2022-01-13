71dcd0724e768d3a8d742fa7688392e9ef5676c3
XINBEIXIAOKEAI <95857482+XINBEIXIAOKEAI@users.noreply.github.com>
2022-01-13 20:15:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -61,22 +61,24 @@ def main():
         gamma = st.sidebar.slider('Gamma 調整', min_value=0.1, max_value=3.0,value=1.0,step=0.1) # change the value here to get different result
         
         Image = st.file_uploader('在這上傳您的檔案',type=['jpg','jpeg','png'])
-        if file_uploader is not None:
+        if Image is not None:
             col1, col2 = st.beta_columns(2)
-            Image = tf.image.decode_image(Image).numpy()
+            Image = Image.read()
+            Image = tf.image.decode_image(Image, channels=3).numpy()                  
             Image = adjust_gamma(Image, gamma=gamma)
             with col1:
                 st.image(Image)
-            text_input = loadtest(Image,cropornot=Autocrop)
-            prediction = comic_model(text_input, training=True)
+            input_image = loadtest(Image,cropornot=Autocrop)
+            prediction = comic_model(input_image, training=True)
             prediction = tf.squeeze(prediction,0)
             prediction = prediction* 0.5 + 0.5
             prediction = tf.image.resize(prediction, 
-                         [outputsize, outputsize],
-                        method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+                           [outputsize, outputsize],
+                           method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
             prediction=  prediction.numpy()
             with col2:
                 st.image(prediction)
+    
 		
     elif choice == 'URL':
         st.sidebar.header('配置')
